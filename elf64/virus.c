ad381c7c5c23dc9e8709a8a127d86c4412fa7375
Ubuntu DigitO <agadhgad@student.42.fr>
2018-09-27 12:05:50 +0000

    add asm bonus

--- elf64/virus.c
+++ elf64/virus.c
@@ -19,22 +19,26 @@ extern char   key[16];
 
 #include <elf.h>
 
+/*
 void decrypt_block(uint32_t* v, uint32_t *k) {
-	uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i;  /* initialisation */
-	uint32_t delta=0x9e3779b9;                     /* constantes de clefs */
-	uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   /* mise en cache de la clef */
-	for (i=0; i<32; i++) {                         /* boucle principale */
+	uint32_t v0=v[0], v1=v[1], sum=0xC6EF3720, i; 
+	uint32_t delta=0x9e3779b9;                     
+	uint32_t k0=k[0], k1=k[1], k2=k[2], k3=k[3];   
+	for (i=0; i<32; i++) {                         
 		v1 -= ((v0<<4) + k2) ^ (v0 + sum) ^ ((v0>>5) + k3);
 		v0 -= ((v1<<4) + k0) ^ (v1 + sum) ^ ((v1>>5) + k1);
 		sum -= delta;
 	}
 	v[0]=v0; v[1]=v1;
 }
+*/
+
+extern void decrypt_block_asm(uint32_t *v, uint32_t *k);
 
 void decrypt(char *s, uint64_t n, uint32_t *k){
 	for (uint64_t i = 0; i < n; i += 8){
 		if (i + 7 <= n){
-			decrypt_block((uint32_t*)(s+i), k);
+			decrypt_block_asm((uint32_t*)(s+i), k);
 		}
 	}
 }
