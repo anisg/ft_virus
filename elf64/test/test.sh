edb0a377c7c496cffe5133a255847283fd7e4f75
ndombre <ndombre@debian.not>
2018-10-02 17:18:29 +0000

    better test

--- elf64/test/test.sh
+++ elf64/test/test.sh
@@ -4,27 +4,34 @@ BIN=../Famine
 
 test_with()
 {
-	cp $1 /tmp/test
-	rm -rf /tmp/test2
-	$BIN # --recure
-	$1 $2 >> out
+	echo "Test" $1 $2
+	cp $1 /tmp/test/test
+	$BIN --recur
+	$1 $2 > out
 
-	cp $1 /tmp/test2
+	cp $1 /tmp/test2/test
 
-	/tmp/test $2 > woody_out
+	echo "  First"
+	/tmp/test/test $2 > woody_out
 	diff out woody_out || exit 1
 
-	/tmp/test2 $2 > woody_out
+	echo "  Second"
+	/tmp/test2/test $2 > woody_out
 	diff out woody_out || exit 1
 
-	#cp /usr/bin/vim /tmp/test2
-	#cp /usr/bin/vim /tmp/test
-	(! diff $1 /tmp/test) || exit 1
-	(! diff $1 /tmp/test2) || exit 1
+	echo "  File"
+	(! diff $1 /tmp/test/test) || exit 1
+	(! diff $1 /tmp/test2/test) || exit 1
 
-	rm -rf /tmp/test2 /tmp/test
+	echo "  Clean"
+	rm -rf /tmp/test2/test /tmp/test/test
 	rm out woody_out
 }
+
+rm -rf /tmp/test
+mkdir /tmp/test
+rm -rf /tmp/test2
+mkdir /tmp/test2
 
 test_with '/bin/tar' '' || exit 1
 test_with '/bin/sh' 'data/sh_script' || exit 1
